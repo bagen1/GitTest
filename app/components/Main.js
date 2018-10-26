0f99754aab15f6458e422a9e17a60187c96aa54b
Karl Johansson <karlj206@gmail.com>
2018-10-26 11:50:13 +0200

    påbörjat createfunktioner

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,12 +1,26 @@
 import React from "react";
-import { StyleSheet, Platform, Image, Text, View, Button,  } from "react-native";
+import { StyleSheet, Platform, Image, Text, View, StatusBar, ListView,  } from "react-native";
 import fire from "../config/config";
-import {Container, Content, Header, Form, Input, Item,Label} from 'native-base';
+import {Container, Content, Header, Form, Input, Item,Label, Button, Icon, List, ListItem} from 'native-base';
+import { database } from "firebase";
 
+var data = ["johan", "johanna"]
 export default class Main extends React.Component {
     state = { currentUser: null };
 
+    constructor(props){
+        super(props);
+
+        this.ds = new ListView.DataSource({rowHasChanged: (r1,r2) => r1 !==r2 })
+
+        this.state = {
+            ListViewData:data,
+            newContact:""
+        }
+    }
+
     componentDidMount() {
+             
         const { currentUser } = fire.auth();
         this.setState({ currentUser });
     }
@@ -15,20 +29,54 @@ export default class Main extends React.Component {
         const { currentUser } = this.state;
 
         return (
-            <View style={styles.container}>
-            <Header>
+            <Container>
+            <Header style={{marginTop:StatusBar.currentHeight }}>
                 <Content>
                     <Item>
                         
-                        <Input>
-                         <Text>Hi {currentUser && currentUser.email}!</Text>
-                         <Button onPress={() => fire.auth().signOut()} title="Logga ut" />
-                        </Input>
+                        
+                         {/* <Text>Hi {currentUser && currentUser.email}!</Text>
+                         <Button onPress={() => fire.auth().signOut()} title="Logga ut" /> */}
+                        <Input
+                        placeholder="Lägg till"
+                        />
+                        <Button>
+                            <Icon name="add"></Icon>
+                        </Button>
                     </Item>
                 </Content>
                 </Header> 
-           </View>
-        );
+                <Content>
+                    
+                  <List
+                  DataSource={this.cloneWithRows(this.state.ListViewData)}
+                  renderRow ={data =>
+                 
+                    <ListItem>
+                        <Text>
+                            {data}
+                        </Text>
+                    </ListItem>
+                  }
+                    renderLeftHiddenRow={data =>
+                    <Button>
+                        <Icon />
+                    </Button>
+                    }
+
+                    renderRightHiddenRow={data =>
+                        <Button>
+                        <Icon />
+                        </Button>
+                    }
+
+                    leftOpenValue={-75}
+                    rightOpenValue={-75}
+                    />
+                  
+                </Content>
+           </Container>
+       );
     }
 }
 const styles = StyleSheet.create({
