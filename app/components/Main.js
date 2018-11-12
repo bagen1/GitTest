59def25157f93e94b81533a155c78616721b83e4
Karl Johansson <karlj206@gmail.com>
2018-11-12 10:48:10 +0100

    clear textinput

--- app/components/Main.js
+++ app/components/Main.js
@@ -10,7 +10,7 @@ export default class Create extends React.Component {
 
     constructor(props) {
         super(props);
-
+        this.searchInput = React.createRef();
         this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
 
         this.state = {
@@ -20,7 +20,6 @@ export default class Create extends React.Component {
     }
 
     componentDidMount() {
-<<<<<<< HEAD
              
             var that = this
             fire.database().ref('/kategori').on('child_added',function(data){
@@ -34,23 +33,12 @@ export default class Create extends React.Component {
         var key =fire.database().ref('/kategori').push().key
         fire.database().ref('/kategori').child(key).set({name:data})
     
-=======
-
-
-        var that = this
-        fire.database().ref('/contacts').on('child_added', function (data) {
-            var newData = [...that.state.listViewData]
-            newData.push(data)
-            that.setState({ listViewData: newData })
-        })
->>>>>>> 22a6dccc373bcb408cd33d1b8399c896039494a3
     }
 
     addRow(data) {
         var key = fire.database().ref('/kategori').push().key
         fire.database().ref('/kategori').child(key).set({ name: data })
 
-<<<<<<< HEAD
     }
     async deleteRow(secId, rowId, rowMap, data) {
         await fire.database().ref('kategori/' + data.key).set(null)
@@ -74,11 +62,13 @@ export default class Create extends React.Component {
                         onChangeText = {(newKategori) => this.setState({newKategori}) }
                         placeholder="Lägg till"
                         placeholderTextColor="#000"
+                        ref={this.searchInput}
                         
                       
                             />
-                        <TouchableOpacity  onPress={() => this.addRow (this.state.newKategori) } > 
+                        <TouchableOpacity  onPress={() => this.addRow (this.state.newKategori) && this.searchInput.current._root.clear()}> 
                             <Icon name="add" style={styles.icon}></Icon>
+
                         </TouchableOpacity>
                        
                     </Item>
@@ -118,82 +108,6 @@ export default class Create extends React.Component {
            </Container>
            
        );
-=======
-
-    }
-
-    addRow(data) {
-        var key = fire.database().ref('/contacts').push().key
-        fire.database().ref('/contacts').child(key).set({ name: data })
-
-    }
-    async deleteRow(secId, rowId, rowMap, data) {
-        await fire.database().ref('contacts/' + data.key).set(null)
-
-
-    }
-
-    render() {
-        const { currentUser } = this.state;
-
-        return (
-            <Container>
-
-                <Header style={{ marginTop: StatusBar.currentHeight }}>
-                    <Content>
-                        <Item>
-
-
-                            {/* <Text>Hi {currentUser && currentUser.email}!</Text>
-                         <Button onPress={() => fire.auth().signOut()} title="Logga ut" /> */}
-                            <Input style={styles.Input}
-                                onChangeText={(newContact) => this.setState({ newContact })}
-                                placeholder="Lägg till"
-                                placeholderTextColor="#000"
-                            />
-                            <TouchableOpacity onPress={() => this.addRow(this.state.newContact)} >
-                                <Icon name={Platform.OS === "ios" ? "ios-add" : "add"} style={styles.icon}></Icon>
-                            </TouchableOpacity>
-
-                        </Item>
-                    </Content>
-                </Header>
-                <Content >
-                    <View>
-                        <List
-                            enableEmptySections
-                            dataSource={this.ds.cloneWithRows(this.state.listViewData)}
-                            renderRow={data =>
-
-                                <ListItem>
-                                    <Text style={styles.Text} >
-                                        {data.val().name}
-                                    </Text>
-                                </ListItem>
-                            }
-                            // renderLeftHiddenRow={data =>
-                            // <Button full onPress={()=> this.addRow(data)}>
-                            //     <Icon name="edit" />
-                            // </Button>
-                            // }
-
-                            renderRightHiddenRow={(data, secId, rowId, rowMap) =>
-                                <Button danger onPress={() => this.deleteRow(secId, rowId, rowMap, data)}>
-                                    <Icon name={Platform.OS === "ios" ? "ios-trash" : "trash"} />
-                                </Button>
-                            }
-
-                            rightOpenValue={-50}
-
-                        />
-                    </View>
-                </Content>
-
-            </Container>
-
-        );
-
->>>>>>> 22a6dccc373bcb408cd33d1b8399c896039494a3
     }
 }
 const styles = StyleSheet.create({
